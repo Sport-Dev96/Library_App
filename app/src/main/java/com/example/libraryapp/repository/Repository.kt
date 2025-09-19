0cd21dc177ef765fac5f4799ee98b46e1988a645
Muslikhiddin <kmuslikhiddin@gmail.com>
2025-09-19 10:37:22 +0300

    improve knowledge

---
+++ app/src/main/java/com/example/libraryapp/repository/Repository.kt
@@ -0,0 +1,19 @@
+package com.example.libraryapp.repository
+
+import com.example.libraryapp.room.BookEntity
+import com.example.libraryapp.room.BooksDB
+
+
+class Repository (val booksDB: BooksDB){
+    suspend fun addBookToRoom(bookEntity: BookEntity){
+        booksDB.bookDao().addBook(bookEntity)
+    }
+     fun getAllBooks() = booksDB.bookDao().getAllBooks()
+    suspend fun deleteBookFromRoom(bookEntity: BookEntity){
+        booksDB.bookDao().deleteBook(bookEntity=bookEntity)
+    }
+    suspend fun updateBook(bookEntity: BookEntity){
+        booksDB.bookDao().updateBook(bookEntity)
+    }
+
+}
